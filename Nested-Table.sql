0839b9f4897a0ba5c0b6d1fafd3274ebca2a7944
Purnikakarki <purnikakarki827@gmail.com>
2026-05-03 09:38:28 +0545

    Added Insert for nested table along with nested CREATE

--- Nested-Table.sql
+++ Nested-Table.sql
@@ -21,6 +21,21 @@ CREATE TYPE address_table AS TABLE OF address_type;
 CREATE TABLE area(
 area_id VARCHAR2(10),
 address address_table)
-NESTED TABLE nested_column_name STORE AS nested_address;
+NESTED TABLE nested_column_name STORE AS nested_address;|
 
+//INSERTING INTO NESTED TABLE
 
+//Insert into nested table (orders)
+
+INSERT INTO orders (order_id, client_id, order_items)
+VALUES (2001, 500,
+    order_item_table_type(
+        order_item_type('DATABASE COURSE', 2000),
+        order_item_type('JAVA COURSE', 1800)
+    )
+);
+
+//Insert into nested table (campaigns)
+
+INSERT INTO campaigns (campaign_id, contacts)
+VALUES (1, contact_table_type('FACEBOOK', 'INSTAGRAM', 'LINKEDIN'));
